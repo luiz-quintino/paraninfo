7b9c520822294d73e6621c4c48689c3356968674
Quintino <luiz.quintino@gmail.com>
2026-01-26 17:27:35 -0300

    up

--- django/_paraninfo/settings.py
+++ django/_paraninfo/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django.context_processors.development_environment",
+                "context_processors.development_environment",
             ],
         },
     },
