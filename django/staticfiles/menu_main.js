0002f9bb9d00ef4e64b33c966234db3c6beddee7
luizquintino <luiz.quintino@stellantis.com>
2026-08-24 13:45:29 -0300

    static files added

---
+++ django/staticfiles/menu_main.js
@@ -0,0 +1,84 @@
+// Seleciona os elementos do menu e do botão
+const menu = document.querySelector('.menu-options');
+const menuButton = document.querySelector('.menu-button');
+
+// Variável para rastrear se o mouse está sobre o menu ou o botão
+let isMouseOverMenuOrButton = false;
+let menuTimeout = 900;
+
+// Alterna o menu ao clicar no botão
+menuButton.addEventListener('click', () => {
+    if (menu.classList.contains('active')) {
+        // Fecha o menu
+        closeMenu();
+    } else {
+        // Abre o menu
+        openMenu();
+    }
+});
+
+// Abre o menu
+function openMenu() {
+    
+    menu.style.display = 'block'; // Torna o menu visível
+    setTimeout(() => {
+        menu.style.opacity = '1'; // Torna o menu opaco
+    }, 10); // Pequeno atraso para ativar a transição
+    menu.classList.add('active');
+    menuButton.classList.add('active'); // Adiciona a classe 'active' ao botão
+    isMouseOverMenuOrButton = true;
+}
+
+// Fecha o menu
+function closeMenu() {
+    isMouseOverMenuOrButton = false;
+    menu.classList.remove('active');
+    menuButton.classList.remove('active'); // Remove a classe 'active' do botão
+    menu.style.opacity = '0'; // Torna o menu transparente
+    setTimeout(() => {
+        menu.style.display = 'none'; // Esconde o menu após a transição
+    }, 500); // Tempo deve coincidir com a transição no CSS
+}
+
+// Mantém o menu aberto enquanto o mouse estiver sobre o menu ou o botão
+menu.addEventListener('mouseenter', () => {
+    isMouseOverMenuOrButton = true;
+});
+
+menu.addEventListener('mouseleave', () => {
+    isMouseOverMenuOrButton = false;
+
+    setTimeout(() => {
+        confirmCloseMenu();
+    }, menuTimeout); // Timeout para evitar fechamento imediato
+});
+
+menuButton.addEventListener('mouseenter', () => {
+    isMouseOverMenuOrButton = true;
+});
+
+
+menuButton.addEventListener('mouseleave', () => {
+    isMouseOverMenuOrButton = false;
+
+    setTimeout(() => {
+        confirmCloseMenu();
+    }, menuTimeout); // Timeout para evitar fechamento imediato
+});
+
+function confirmCloseMenu() {
+    if (!isMouseOverMenuOrButton) {
+        closeMenu();
+    }
+}
+
+
+// Ampulheta
+function showLoadingOverlay() {
+    // Exibe a ampulheta
+    document.getElementById('loading-overlay').style.display = 'block';
+}
+
+function hideLoadingOverlay() {
+    document.getElementById('loading-overlay').style.display = 'none';
+}
